5ea10630673444e3633622df21c5241acbe93140
Ricardo Santos <riccardhos@gmail.com>
2025-06-01 13:05:15 -0300

    Projeto WEB: Clínica Med Plus - API

---
+++ src/test/java/br/com/ifpe/medplus_api/MedplusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.medplus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedplusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
